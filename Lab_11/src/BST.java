839258aa1c943c5de71447f681811dc100c15cf6
Nguyễn Ngọc Thịnh <thinh23022004@gmail.com>
2023-12-05 21:55:10 +0700

    lab11

--- Lab_11/src/BST.java
+++ Lab_11/src/BST.java
@@ -3,6 +3,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+import javax.naming.spi.DirStateFactory.Result;
+
 public class BST<E extends Comparable<E>> {
 	private BNode<E> root;
 
@@ -100,74 +102,132 @@ public class BST<E extends Comparable<E>> {
 	}
 
 	// compute the height of BST
-//	public int height() {
-//		// TODO
-//		if (root == null) return 0 ;
-//		Queue<BNode<E>> queue = new LinkedList<>();
-//		queue.add(root);
-//		queue.add(null);
-//		int depth = 0;
-//		while (!queue.isEmpty()) {
-//			BNode<E> tmp = queue.peek() ;
-//			queue.remove() ;
-//			
-//			
-//			if (tmp== null) {
-//				depth ++ ;
-//			}
-//			if (tmp!=null) {
-//				if (tmp.getLeft()!= null) {
-//					queue.add(tmp.getLeft());
-//				}
-//				if (tmp.getRight()!= null) {
-//					queue.add(tmp.getRight());
-//				}
-//			}else if (!queue.isEmpty()) {
-//				queue.add(null);
-//			}
-//		}
-//		return depth ;
-//	}
 	public int height() {
-		if (root == null ) return 0 ;
+		if (root == null)
+			return 0;
+
 		Queue<BNode<E>> queue = new LinkedList<>();
 		queue.add(root);
 		int depth = 0;
-		
+
 		while (!queue.isEmpty()) {
-			
+			BNode<E> tmp = queue.peek();
+			queue.remove();
+			if (tmp == null) {
+				depth++;
+			}
+			if (tmp != null) {
+				if (tmp.getLeft() != null) {
+					queue.add(tmp.getLeft());
+
+				}
+				if (tmp.getRight() != null) {
+					queue.add(tmp.getRight());
+				}
+			} else {
+				if (!queue.isEmpty()) {
+					queue.add(null);
+				}
+			}
 		}
-		
-		
+
+		return depth;
 	}
+
 	// Compute total nodes in BST
 	public int size() {
-		// TODO
-		return -1;
+		return getSize(root);
+	}
+
+	private int getSize(BNode<E> currentNode) {
+		if (currentNode == null)
+			return 0;
+		return 1 + getSize(currentNode.getLeft()) + getSize(currentNode.getRight());
 	}
 
 	// Check whether element e is in BST
 	public boolean contains(E e) {
 		// TODO
+		if (root == null)
+			return false;
+		BNode<E> temp = root;
+		while (temp != null) {
+			if (e.compareTo(temp.getData()) == 0) {
+				return true;
+			} else if (e.compareTo(temp.getData()) > 0) {
+				temp = temp.getRight();
+			} else {
+				temp = temp.getLeft();
+			}
+		}
 		return false;
 	}
 
 	// Find the minimum element in BST
 	public E findMin() {
-		// TODO
-		return null;
+		if (root == null)
+			return null;
+		BNode<E> temp = root;
+		while (temp.getLeft() != null) {
+			temp = temp.getLeft();
+		}
+		return temp.getData();
 	}
 
 	// Find the maximum element in BST
 	public E findMax() {
-		// TODO
-		return null;
+		if (root == null)
+			return null;
+		BNode<E> temp = root;
+		while (temp.getRight() != null) {
+			temp = temp.getRight();
+		}
+		return temp.getData();
 	}
 
 	// Remove element e from BST
 	public boolean remove(E e) {
-		// TODO
-		return false;
+		if (root == null || contains(e))
+			return false;
+		root = helperRemove(root, e);
+		return true;
+
+	}
+
+	private BNode<E> helperRemove(BNode<E> currentNode, E value) {
+		if (currentNode == null)
+			return null;
+		if (value.compareTo(currentNode.getData()) > 0) {
+			currentNode.setRight(helperRemove(currentNode.getRight(), value));
+		} else {
+			if (value.compareTo(currentNode.getData()) < 0) {
+
+				currentNode.setLeft(helperRemove(currentNode.getLeft(), value));
+			} else {
+				if (currentNode.getLeft() == null && currentNode.getRight() == null)
+					return null;
+
+				if (currentNode.getLeft() != null && currentNode == null)
+					return currentNode.getLeft();
+
+				if (currentNode.getLeft() == null && currentNode.getRight() != null)
+					return currentNode.getRight();
+				BNode<E> leftMostNode = findLeftModeNode(currentNode.getRight());
+				currentNode = leftMostNode;
+				currentNode.setRight(helperRemove(currentNode.getRight(), leftMostNode.getData()));
+			}
+		}
+
+	}
+
+	private BNode<E> findLeftModeNode(BNode<E> cur) {
+		if (cur == null)
+			return null;
+		BNode<E> leftMostNode = cur;
+		while (leftMostNode.getLeft() != null) {
+			leftMostNode = leftMostNode.getLeft();
+		}
+		return leftMostNode;
 	}
 
 	// get the descendants of a node
