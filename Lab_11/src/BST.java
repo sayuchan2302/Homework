361c2795474842c1492b8a372e29fb9110c1aed7
Nguyễn Ngọc Thịnh <thinh23022004@gmail.com>
2023-12-05 16:19:22 +0700

    lab11

--- Lab_11/src/BST.java
+++ Lab_11/src/BST.java
@@ -1,3 +1,7 @@
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class BST<E extends Comparable<E>> {
 	private BNode<E> root;
@@ -10,65 +14,171 @@ public class BST<E extends Comparable<E>> {
 		return root;
 	}
 
+	// Add element e into BST
 	public void add(E e) {
-		BNode<E> value = new BNode<>(e);
+		BNode<E> newValue = new BNode<>(e);
 		if (root == null) {
-			root = value;
-			System.out.println("add val to root");
+			root = newValue;
+			System.out.println("add value to root");
 		} else {
 			BNode<E> temp = root;
 			while (true) {
 				if (e.compareTo(temp.getData()) > 0) {
 					if (temp.getRight() == null) {
-						System.out.println("add val to right " + temp.getData());
-						temp.setRight(value);
+						System.out.println("add value " + e + " to right: " + temp.getData());
+						temp.setRight(newValue);
 						break;
 					} else {
 						temp = temp.getRight();
 					}
 				} else {
 					if (temp.getLeft() == null) {
-						System.out.println("add val to left " + temp.getData());
-						temp.setLeft(value);
+						System.out.println("add value " + e + " to left: " + temp.getData());
+						temp.setRight(newValue);
 						break;
 					} else {
 						temp = temp.getLeft();
-
 					}
 				}
 			}
+
+		}
+	}
+
+	// Add a collection of elements col into BST
+	public void add(Collection<E> col) {
+		// TODO
+		for (E e : col) {
+			add(e);
 		}
 
 	}
 
-//	private void addRecursive (BNode<E> current , E e){
-//		if (current == null) {
-//		}
-//		if (e.compareTo(current.getData()) >0) {
-//			if (current.getRight() == null) {
-//				current.setRight(e);
+	// compute the depth of a node in BST
+	public int depth(E node) {
+		// TODO
+		BNode<E> currentNode = root;
+		int currentDepth = 0;
+		while (currentNode != null) {
+			if (currentNode.getData().equals(node)) {
+				return currentDepth;
+			} else {
+				if (currentNode.getData().compareTo(node) > 0) {
+					currentNode = currentNode.getLeft();
+				} else {
+					currentNode = currentNode.getRight();
+				}
+			}
+			currentDepth++;
+		}
+		return -1;
+	}
+
+	public int depthUsingRecursive(E e) {
+		return calcalateDepth(root, e);
+	}
+
+	private int calcalateDepth(BNode<E> currentNode, E value) {
+		if (currentNode == null) {
+			return -1;
+		}
+		if (currentNode.getData().equals(value)) {
+			return 0;
+		}
+
+		int leftDepth = calcalateDepth(currentNode.getLeft(), value);
+		if (leftDepth != -1) {
+			return leftDepth + 1;
+		}
+
+		int rightDepth = calcalateDepth(currentNode.getRight(), value);
+		if (rightDepth != -1) {
+			return rightDepth + 1;
+		}
+
+		return -1;
+	}
+
+	// compute the height of BST
+//	public int height() {
+//		// TODO
+//		if (root == null) return 0 ;
+//		Queue<BNode<E>> queue = new LinkedList<>();
+//		queue.add(root);
+//		queue.add(null);
+//		int depth = 0;
+//		while (!queue.isEmpty()) {
+//			BNode<E> tmp = queue.peek() ;
+//			queue.remove() ;
+//			
+//			
+//			if (tmp== null) {
+//				depth ++ ;
 //			}
-//			addRecursive(current.getRight(), e);
-//		}else {
-//			addRecursive(current.getLeft(), e);
+//			if (tmp!=null) {
+//				if (tmp.getLeft()!= null) {
+//					queue.add(tmp.getLeft());
+//				}
+//				if (tmp.getRight()!= null) {
+//					queue.add(tmp.getRight());
+//				}
+//			}else if (!queue.isEmpty()) {
+//				queue.add(null);
+//			}
 //		}
+//		return depth ;
 //	}
-	public void addUsingRecursive(E e) {
-		root = addRecursive(root, e);
+	public int height() {
+		if (root == null ) return 0 ;
+		Queue<BNode<E>> queue = new LinkedList<>();
+		queue.add(root);
+		int depth = 0;
+		
+		while (!queue.isEmpty()) {
+			
+		}
+		
+		
+	}
+	// Compute total nodes in BST
+	public int size() {
+		// TODO
+		return -1;
 	}
 
-	private BNode<E> addRecursive(BNode<E> current, E e) {
-		if (current == null) {
-			return new BNode<>(e);
-		}
-
-		if (e.compareTo(current.getData()) > 0) {
-			current.setRight(addRecursive(current.getRight(), e));
-		} else if (e.compareTo(current.getData()) < 0) {
-			current.setLeft(addRecursive(current.getLeft(), e));
-		}
-
-		return current;
+	// Check whether element e is in BST
+	public boolean contains(E e) {
+		// TODO
+		return false;
 	}
 
+	// Find the minimum element in BST
+	public E findMin() {
+		// TODO
+		return null;
+	}
+
+	// Find the maximum element in BST
+	public E findMax() {
+		// TODO
+		return null;
+	}
+
+	// Remove element e from BST
+	public boolean remove(E e) {
+		// TODO
+		return false;
+	}
+
+	// get the descendants of a node
+	public List<E> descendants(E data) {
+		// TODO
+		return null;
+	}
+
+	// get the ancestors of a node
+	public List<E> ancestors(E data) {
+		// TODO
+		return null;
+	}
 }
